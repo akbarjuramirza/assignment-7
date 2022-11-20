4ec68da519923edf1d2964a560e769959971cb99
akbarjuramirza <118677024+akbarjuramirza@users.noreply.github.com>
2022-11-20 20:25:24 +0500

    Update problem_1.cpp

--- problem_1.cpp
+++ problem_1.cpp
@@ -1,7 +1,5 @@
 /*Write C++ program that has a function multiply with two parameters:x and y. This function should perform simple multiplication, but only with ”+” and ”-” operators using recursion. Call this function from the main and print the result; x and y are user inputs. DO NOT USE ”*” OPERATOR.*/
 //RPOBLEM-1
-//Full-name: Juramirzaev Akbarjon
-//ID: 210014
 
 #include <iostream>
 
